5a23ee307391719ec886b3b1f7e870a300d6b6d1
Jshyamsundar <143583521+Jshyamsundar@users.noreply.github.com>
2023-12-12 15:49:28 +0530

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,19 +1,19 @@
-const toggle = document.getElementById("toggleDark");
-const body = document.querySelector("body");
+// const toggle = document.getElementById("toggleDark");
+// const body = document.querySelector("body");
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle("fa-moon");
-    if(this.classList.toggle("fa-sun")){
-        body.style.background = "white";
-        body.style.color = "black";
-        body.style.transition = "2s";
+// toggle.addEventListener('click', function(){
+//     this.classList.toggle("fa-moon");
+//     if(this.classList.toggle("fa-sun")){
+//         body.style.background = "white";
+//         body.style.color = "black";
+//         body.style.transition = "2s";
 
-    }
-    else{
-        body.style.background = "black";
-        body.style.color = "white";
-        body.style.transition= "2s";
+//     }
+//     else{
+//         body.style.background = "black";
+//         body.style.color = "white";
+//         body.style.transition= "2s";
 
-    }
+//     }
 
-})
+// })
